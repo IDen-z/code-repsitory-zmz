1ed27f08e4726c11abe5c675b8d5776335efb1c6
IDen <zmzxdal@foxmail.com>
2021-06-07 14:37:43 +0800

    新增LocalDate项目代码20210607

---
+++ localdatetime/src/test/java/DateTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println("======================");
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getYear()+1900);
+        System.out.println(date.getMonth()+1);
+
+        System.out.println("======================");
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate.getMonthValue());
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getDayOfMonth());
+        System.out.println("======================");
+        LocalDate localDate1 = LocalDate.of(2021, 11, 11);
+        System.out.println(localDate1);
+        System.out.println("======================");
+        LocalDateTime localDateTime= LocalDateTime.now();
+        System.out.println(localDateTime);
+        String formatTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+        System.out.println(formatTime);
+
+    }
+
+
+
+}
